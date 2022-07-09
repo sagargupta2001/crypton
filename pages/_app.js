b14a0341fa9ecbc83c1032555e8be8222a6d917d
sagar <sagargupta201@outlook.com>
2022-07-09 09:16:15 +0530

    Deployed smart contract.

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,24 @@
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 
-import { Navbar, Footer } from '../components';
-
+import { NFTProvider } from '../context/NFTContext';
+import { Footer, Navbar } from '../components';
 import '../styles/globals.css';
 
 const Marketplace = ({ Component, pageProps }) => (
-
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-65">
-        <Component {...pageProps} />
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
 
-    <Script src="https://kit.fontawesome.com/e870816ced.js" crossOrigin="anonymous" />
-  </ThemeProvider>
-
+      <Script src="https://kit.fontawesome.com/d45b25ceeb.js" crossorigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default Marketplace;
