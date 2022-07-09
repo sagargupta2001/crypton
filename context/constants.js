953eca6cb366d2f4be95c0ff3a436b88af38b484
NAMA SAGAR GUPTA <37893527+sagargupta2001@users.noreply.github.com>
2022-07-09 23:56:53 +0530

    Update constants.js

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import market from './NFTMarketplace.json';
 
-export const MarketAddress = '0x91555f97da66C8719ea6E2C4b7E9a3c36a1e8EAc';
+export const MarketAddress = '0x2Fe39eE4Cc6F5C6dfe7757f343AE45F272aa0578';
 export const MarketAddressABI = market.abi;
