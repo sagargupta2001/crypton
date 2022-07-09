a1f06229e3b5458e1a01c8ee85026922d5bb79b4
sagar <sagargupta201@outlook.com>
2022-07-09 13:23:35 +0530

    Add top creators functionality.

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import { NFTContext } from '../context/NFTContext';
 import { getCreators } from '../utils/getTopCreators';
 import { shortenAddress } from '../utils/shortenAddress';
 import images from '../assets';
-import { makeid } from '../utils/makeId';
 
 const Home = () => {
   const { fetchNFTs } = useContext(NFTContext);
@@ -126,7 +125,7 @@ const Home = () => {
                       creatorEths={creator.sumall}
                     />
                   ))}
-                  {[6, 7, 8, 9, 10].map((i) => (
+                  {/* [6, 7, 8, 9, 10].map((i) => (
                     <CreatorCard
                       key={`creator-${i}`}
                       rank={i}
@@ -134,7 +133,7 @@ const Home = () => {
                       creatorName={`0x${makeid(3)}...${makeid(4)}`}
                       creatorEths={10 - i * 0.534}
                     />
-                  ))}
+                  )) */}
                   {!hideButtons && (
                     <>
                       <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
