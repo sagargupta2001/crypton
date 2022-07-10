6153675cffd831b4d67f02fd5199dfe1ef757edd
sagar <sagargupta201@outlook.com>
2022-07-10 07:57:50 +0530

    Bug Fixes

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,8 @@ module.exports = {
     // rinkeby: {
     // url: 'https://rinkeby.infura.io/v3/bed4fdcc76bb4978a9a3103ef0946f64',
     //   accounts: [privateKey],
+    // https://rpc-mumbai.maticvigil.com
+    // https://matic-mumbai.chainstacklabs.com
     // },
   },
   solidity: '0.8.4',
