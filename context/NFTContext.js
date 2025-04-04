94515cd352584b0c4676ef2a1da7a4cf05131503
NAMA SAGAR GUPTA <37893527+sagargupta2001@users.noreply.github.com>
2025-04-04 12:52:05 +0530

    Update NFTContext.js

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -17,7 +17,7 @@ export const NFTProvider = ({ children }) => {
   const fetchNFTs = async () => {
     setIsLoadingNFT(false);
 
-    const provider = new ethers.providers.JsonRpcProvider('https://endpoints.omniatech.io/v1/matic/mumbai/public');
+    const provider = new ethers.providers.JsonRpcProvider('https://polygontestapi.terminet.io/rpc');
     const contract = fetchContract(provider);
 
     const data = await contract.fetchMarketItems();
